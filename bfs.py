caaeebfae59b9d66616241d4f35eed8893ab8d24
r1cardohj <2413302357@qq.com>
2025-05-25 17:06:05 +0800

    improve bfs

--- bfs.py
+++ bfs.py
@@ -7,27 +7,30 @@ graph["bob"] = "anuj", "peggy"
 graph["alice"] = "peggy",
 graph["claire"] = "thom", "jonny"
 graph["anuj"] = tuple()
-graph["peggy"] = tuple()
+graph["peggy"] = "you",
 graph["thom"] = tuple()
 graph["jonny"] = tuple()
 
-search_queue = deque()
-search_queue += graph["you"]
 
 def person_is_seller(name):
     return name[-1] == "m"
 
-def search():
-    global search_queue
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = set()
     while search_queue:
         person = search_queue.popleft()
-        print(person)
-        if person_is_seller(person):
-            print(person + " is a mango seller")
-            return True
-        else:
-            search_queue += graph[person]
+        if person not in searched:
+            print(person)
+            if person_is_seller(person):
+                print(person + " is a mango seller")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.add(person)
     return False
 
 
-search()
+search("you")
